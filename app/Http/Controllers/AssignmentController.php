54ddb2ded2844d737d8bfb5d140f00b9cd87dfd1
Sziklai Dávid <szikla12david@gmail.com>
2020-05-29 23:50:39 +0200

    comiiiiiit

--- app/Http/Controllers/AssignmentController.php
+++ app/Http/Controllers/AssignmentController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Assignment;
 use Illuminate\Http\Request;
 
 class AssignmentController extends Controller
 {
-    //
+    public function store(Request $request, $id){
+        $validatedData = $request->validate([
+            'name' => ['required', 'min:5'],
+            'desc' => ['require'],
+        ]);
+
+        $assignment = new Assignment;
+
+        $assignment->name = $request->name;
+        $assignment->desc = $request->desc;
+        $assignment->value = $request->value;
+        $assignment->deadline_from = $request->deadline_from;
+        $assignment->deadline_to = $request->deadline_to;
+        $assignment->subject = Auth::id();
+
+        $assignment->save();
+
+        return redirect(route('teacher'));
+    }
 }
